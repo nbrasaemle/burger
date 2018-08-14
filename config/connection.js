4af3dc32a2e2a10cb0a0af842aba5c0abcf04fcd
nbrasaemle <nicole.brasaemle@gmail.com>
2018-08-13 23:04:47 -0500

    update the connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,27 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "Localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  conection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: "Localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
+ connection.connect();
+
+// var connection = mysql.createConnection({
+//   host: "Localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
 
 connection.connect(function(err) {
   if (err) {
